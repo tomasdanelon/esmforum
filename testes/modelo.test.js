8917a2235aab2b23156ec99f42df2b571941f938
tomasdanelon <tomasdanelon@gmail.com>
2022-11-27 11:41:52 -0300

    novos testes adicionados

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,26 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+test('Testando cadastro de três respostas', () => {
+  modelo.cadastrar_resposta(0, '2');
+  modelo.cadastrar_resposta(1, '4');
+  modelo.cadastrar_resposta(2, '6');
+  const resposta = modelo.get_respostas(0)
+  const resposta1 = modelo.get_respostas(1)
+  const resposta2 = modelo.get_respostas(2)
+  expect(resposta[0].texto).toBe('2')
+  expect(resposta1[0].texto).toBe('4')
+  expect(resposta2[0].texto).toBe('6')
+});
+test('Testando get perguntas', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_pergunta('2 + 2 = ?');
+  modelo.cadastrar_pergunta('3 + 3 = ?');
+  const perguntas = modelo.listar_perguntas(); 
+  const pergunta = modelo.get_pergunta(perguntas[0].id_pergunta)
+  const pergunta1 = modelo.get_pergunta(perguntas[1].id_pergunta)
+  const pergunta2 = modelo.get_pergunta(perguntas[2].id_pergunta)
+  expect(pergunta.texto).toBe('1 + 1 = ?')
+  expect(pergunta1.texto).toBe('2 + 2 = ?')
+  expect(pergunta2.texto).toBe('3 + 3 = ?')
+})
